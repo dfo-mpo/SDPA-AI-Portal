2089af6e0f3880aad7afbc05c77b0c7b25caeb5e
David Onak <54957341+DavidOnak@users.noreply.github.com>
2024-12-30 12:54:17 -0800

    Clean out old files from Django project

--- CSV_PDF_Analyzer_App/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from CSV_PDF_Analyzer_App.models import CSVUpload
-
-class CSVUploadForm(forms.ModelForm):
-    class Meta:
-        model = CSVUpload
-        fields = ('csv_file',)
