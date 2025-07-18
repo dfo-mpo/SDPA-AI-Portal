3e070c841fa5958d301b7020bb062d7407f0e215
haydenDFO <162184368+haydenDFO@users.noreply.github.com>
2025-07-18 10:39:42 -0700

    Adjust required option to form components. Adjust radio/checkbox elements spacing

--- src/pages/SurveyForm/RadioGroup.jsx
+++ src/pages/SurveyForm/RadioGroup.jsx
@@ -3,7 +3,7 @@ export default function RadioGroup({ name, label, description, value, onChange, 
     <div className="mb-4">
       <label className="block text-black font-bold mb-2 dark:text-white">{label}</label>
       {description && (<div className="text-sm text-black mb-4 dark:text-white">{description}</div>)}
-      <div className="flex space-x-6">
+      <div className="flex flex-wrap gap-6">
         {options.map((option, index) => (
           <label key={index} className="flex items-center cursor-pointer">
             <input 
