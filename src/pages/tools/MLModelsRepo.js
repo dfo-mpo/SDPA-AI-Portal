1db23e8e11cb1597f76f11e5ec63ee9cfd87a207
Potiagalov <Alexander.Potiagalov@dfo-mpo.gc.ca>
2025-09-15 13:26:42 -0700

    Added banner, title, and description to MlModelRepo page.

--- src/pages/tools/MLModelsRepo.js
+++ src/pages/tools/MLModelsRepo.js
@@ -7,12 +7,28 @@
  * metadata, uploading new models, and monitoring usage statistics.
  */
 
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { ToolPage } from '../../components/tools';
+import { useLanguage, useToolSettings } from '../../contexts';
+import { getToolTranslations } from '../../utils';
+import { useIsAuthenticated } from '@azure/msal-react';
 
 export function MLModelsRepo() {
+  const { language } = useLanguage();
+  const toolData = getToolTranslations("mlModelsRepo", language);
+  const isAuth = useIsAuthenticated();
 
-
-  return (<></>);
+  return (
+    <ToolPage
+      // Use the uploadKey to force re-creation of the file input when needed
+      title={toolData.title}
+      shortDescription={toolData.shortDescription}
+      longDescription={toolData.longDescription}
+      backgroundImage="/assets/calculations.png"
+      hideActionButton
+    >
+    </ToolPage>
+  );
 }
 
 export default MLModelsRepo;
