ed06888218e6cd72dd3b9bac21744c1055ec1565
David Onak <David.Onak@dfo-mpo.gc.ca>
2025-09-25 12:05:09 -0700

    Updating api path to server

--- src/pages/tools/MLModelsRepo/services/repoApi.js
+++ src/pages/tools/MLModelsRepo/services/repoApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = 'http://localhost:4000/api'; // or use your proxy/env
+const API = 'server'; // or use your proxy/env
 
 /* ---------- Models ----------
 - CRUD Operations for models
