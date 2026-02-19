18d08c72ef5648b39b09e49f300ec6e22d2442b1
Eldesouki <Ahmed.Eldesouki@dfo-mpo.gc.ca>
2026-02-19 13:34:53 -0500

    Added Test category and object model detection tool

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -206,6 +206,19 @@ import {
       actionText: 'Coming Soon',
       disabled: true
     },
+
+    OBJECT_MODEL_DETECTION: {
+      id: 'ahmed-website',
+      name: "Object Model Detection",
+      category: 'Test',
+      icon: Eye, 
+      backgroundImage: '/assets/ahmed-website-banner.jpg',
+      actionText: 'Open Website',
+      disabled: true,
+      showInDemo: false
+    },
+
+    
   };
   
   /**
@@ -245,6 +258,9 @@ import {
     ],
     'AI Resources': [
       TOOLS.AI_RESOURCES,
+    ],
+    'Test': [
+      TOOLS.OBJECT_MODEL_DETECTION,
     ]
   };
   
